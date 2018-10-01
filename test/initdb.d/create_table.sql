0682094b7363dd975023ed6939a6769e3f4fc857
onigra <3280467rec@gmail.com>
2018-10-01 20:04:02 +0900

    add testdata

---
+++ test/initdb.d/create_table.sql
@@ -0,0 +1,13 @@
+use sample;
+
+CREATE TABLE IF NOT EXISTS tasks(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  memo text
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
+;
+
+INSERT INTO tasks VALUES
+  (1, 'foo'),
+  (2, 'bar'),
+  (3, 'baz')
+;
